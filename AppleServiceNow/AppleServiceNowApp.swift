b486edd2f9ba3642643874e224fc077bfc7fda46
SvenFE <shensven2016@gmail.com>
2022-08-19 17:14:29 +0800

    Initial Commit

---
+++ AppleServiceNow/AppleServiceNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleServiceNowApp.swift
+//  AppleServiceNow
+//
+//  Created by SvenFE on 2022/8/19.
+//
+
+import SwiftUI
+
+@main
+struct AppleServiceNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
